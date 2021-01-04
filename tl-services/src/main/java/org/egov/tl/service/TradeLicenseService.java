adf4d93759d7d324568b0f46a7b2e62fbb968ba8
cscl-git <57174349+cscl-git@users.noreply.github.com>
2021-01-04 15:29:42 +0000

    updated conf

--- tl-services/src/main/java/org/egov/tl/service/TradeLicenseService.java
+++ tl-services/src/main/java/org/egov/tl/service/TradeLicenseService.java
@@ -322,7 +322,10 @@ public class TradeLicenseService {
             }
             enrichmentService.postStatusEnrichment(tradeLicenseRequest,endStates,mdmsData);
             userService.createUser(tradeLicenseRequest, false);
-            calculationService.addCalculation(tradeLicenseRequest);
+			if(businessServicefromPath!=null && !businessServicefromPath.equals(businessService_BPA))
+			{
+				calculationService.addCalculation(tradeLicenseRequest);
+			}
             switch (businessServicefromPath) {
                 case businessService_TL:
                     editNotificationService.sendEditNotification(tradeLicenseRequest, diffMap);
