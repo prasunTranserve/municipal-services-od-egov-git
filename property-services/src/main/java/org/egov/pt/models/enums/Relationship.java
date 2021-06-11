4bade7b623f163465a03d6ff53f016d35caffb6d
Prasun Naiya <prasun.naiya@pwc.com>
2021-06-11 19:22:20 +0530

    Relationship added

--- property-services/src/main/java/org/egov/pt/models/enums/Relationship.java
+++ property-services/src/main/java/org/egov/pt/models/enums/Relationship.java
@@ -10,7 +10,14 @@ import com.fasterxml.jackson.annotation.JsonValue;
 public enum Relationship {
 	
 	FATHER("FATHER"),
-    HUSBAND("HUSBAND");
+    HUSBAND("HUSBAND"),
+    MOTHER("MOTHER"),
+    BROTHER("BROTHER"),
+    SISTER("SISTER"),
+    GRANDFATHER("GRANDFATHER"),
+    GRANDMOTHER("GRANDMOTHER"),
+    FATHERINLAW("FATHERINLAW"),
+    MOTHERINLAW("MOTHERINLAW");
 
   private String value;
 
