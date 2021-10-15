52aa59e1cfb78b664167bb60182853e87678cd32
Prasun Naiya <prasun.naiya@pwc.com>
2021-10-15 18:05:03 +0530

    WNS rebate and penalty issue fix

--- ws-calculator/src/main/java/org/egov/wscalculation/service/MasterDataService.java
+++ ws-calculator/src/main/java/org/egov/wscalculation/service/MasterDataService.java
@@ -8,6 +8,7 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -318,8 +319,10 @@ public class MasterDataService {
 			LocalDate today = LocalDate.now();
 			int month = today.getMonthValue();
 			int year = today.getYear();
-			date = new Date(year, month, Integer.parseInt(startDay));
-			return date.getTime();
+			Calendar cal = Calendar.getInstance();
+			cal.clear();
+			cal.set(year, month-1, Integer.parseInt(startDay));
+			return cal.getTimeInMillis();
 		}
 		return date.getTime();
 	}
