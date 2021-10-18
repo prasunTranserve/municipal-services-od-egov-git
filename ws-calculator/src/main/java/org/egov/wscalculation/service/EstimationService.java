4e8c15bed2ebad2c111a85d28fb40a779e21b734
Prasun Naiya <prasun.naiya@pwc.com>
2021-10-18 18:57:15 +0530

    Fix billing period for wns from scheduler

--- ws-calculator/src/main/java/org/egov/wscalculation/service/EstimationService.java
+++ ws-calculator/src/main/java/org/egov/wscalculation/service/EstimationService.java
@@ -423,10 +423,10 @@ public class EstimationService {
 	}
 
 	private static void setTimeToEndofDay(Calendar calendar) {
-	    calendar.set(Calendar.HOUR_OF_DAY, 23);
-	    calendar.set(Calendar.MINUTE, 59);
-	    calendar.set(Calendar.SECOND, 59);
-	    calendar.set(Calendar.MILLISECOND, 999);
+	    calendar.set(Calendar.HOUR_OF_DAY, 18);
+	    calendar.set(Calendar.MINUTE, 29);
+	    calendar.set(Calendar.SECOND, 0);
+	    calendar.set(Calendar.MILLISECOND, 0);
 	}
 	
 	
