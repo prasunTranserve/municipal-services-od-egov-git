2a7777ddf46a5777f3ff2ed9dd16117a6b4233e9
arkitdas-pwc <arkit.das@pwc.com>
2022-02-28 17:29:03 +0000

    Add loggers for penalty implementation

--- pt-calculator-v2/src/main/java/org/egov/pt/calculator/service/DemandService.java
+++ pt-calculator-v2/src/main/java/org/egov/pt/calculator/service/DemandService.java
@@ -442,6 +442,8 @@ public class DemandService {
 		//Checks if the current date id within demand from and to date 
 		if((demand.getTaxPeriodFrom()<= System.currentTimeMillis() && demand.getTaxPeriodTo() >= System.currentTimeMillis()))
 			isCurrentDemand = true;
+		
+		log.debug("isCurrentDemand ["+isCurrentDemand+"]");
 		/*
 		 * method to get the latest collected time from the receipt service
 		 */
@@ -476,6 +478,8 @@ public class DemandService {
 			penalty = payService.applyPenalty(taxAmtForPenalty,collectedPtTax,
 	                taxPeriod.getFinancialYear(), timeBasedExmeptionMasterMap,payments,taxPeriod);
 		}
+		
+		log.debug("Penalty Amount ["+penalty+"]");
 		
 		rebate = payService.applyRebate(taxAmt, collectedPtTax, taxPeriod.getFinancialYear(),
 				timeBasedExmeptionMasterMap, payments, taxPeriod);
